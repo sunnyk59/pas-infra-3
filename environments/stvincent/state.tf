34b4e669bf6eda99def9ce7a0e7048f3f6ec40ae
sunnyk59 <sunny.aus59@gmail.com>
2025-03-04 14:47:39 +1100

    first commit

---
+++ environments/stvincent/state.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.41.0" 
+    }
+  }
+  backend "s3" {
+    bucket         = "webpas-terraform-state-stvincent"
+    key            = "terraform/environments/stvincent/terraform.tfstate"  
+    region         = "ap-southeast-2"
+    dynamodb_table = "webpas-terraform-state-lock"
+    encrypt        = true
+  }
+}
